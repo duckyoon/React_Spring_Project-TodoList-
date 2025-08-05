f11de88fbb4253371699a027d507a14473623933
Yoonduck <dkdlel616@gmail.com>
2025-08-05 23:18:33 +0900

    FEAT: (WebMvcConfigurer 설정) --- 리액터, 스프링 첫 연동 ---

---
+++ backend/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,29 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				WebMvcConfigurer.super.addCorsMappings(registry);
+				registry.addMapping("/**") // 모든 경로 허용
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:5173");
+			}
+		};
+	}
+}
